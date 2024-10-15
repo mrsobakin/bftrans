697de6628845007ca97fdee2ea1afdca9c1f49a2
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2024-10-15 21:42:19 +0300

    bflabels: Add more short label & scope tests

--- tests/bflabels_compiler.cpp
+++ tests/bflabels_compiler.cpp
@@ -22,3 +22,76 @@ TEST(BFLabelsCompiler, Simple) {
 
     ASSERT_THAT(result, AnyOf("+>+", ">+<+"));
 }
+
+TEST(BFLabelsCompiler, LabelElision) {
+    using namespace bflabels;
+    using namespace ::testing;
+
+    Tokens code = {
+        '+',
+        Label(1, 0),
+        Label(2, 0),
+        Label(2, 0),
+        Label(3, 0),
+        Label(1, 0),
+        Label(2, 0),
+        Label(1, 0),
+        Label(3, 0),
+        Label(1, 0),
+        '+',
+    };
+
+    auto result = BFLCode(code).compile();
+
+    ASSERT_THAT(result, AnyOf("++", "+>+", "+>>+"));
+}
+
+TEST(BFLabelsCompiler, InterscopeLabelReuse) {
+    using namespace bflabels;
+    using namespace ::testing;
+
+    Tokens code = {
+        '+',
+        Scope::Enter,
+        Label(1, 0),
+        '+',
+        Scope::Exit,
+        Scope::Enter,
+        Label(2, 0),
+        '+',
+        Scope::Exit,
+        Scope::Enter,
+        Label(3, 0),
+        '+',
+        Scope::Exit,
+        '+',
+    };
+
+    auto result = BFLCode(code).compile();
+
+    ASSERT_THAT(result, AnyOf("+++++"));
+}
+
+TEST(BFLabelsCompiler, IntrascopeLabelSharing) {
+    using namespace bflabels;
+    using namespace ::testing;
+
+    Tokens code = {
+        '+',
+        Scope::Enter,
+        Label(1, 0),
+        '+',
+        Label(2, 0),
+        '+',
+        Scope::Enter,
+        Label(1, 0),
+        '+',
+        Scope::Exit,
+        Scope::Exit,
+        '+',
+    };
+
+    auto result = BFLCode(code).compile();
+
+    ASSERT_THAT(result, AnyOf("+>+<+>++", "++>+<+"));
+}
